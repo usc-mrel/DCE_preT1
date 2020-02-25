e9bee02b8fdbd0a1e03f148149d464af625352d9
Zhibo Zhu <zhibozhu@usc.edu>
2020-02-25 11:49:06 -0800

    Updated
    Fourier Transforms are aligned with STARDCE functions.

--- Direct_T1/Direct_demo.m
+++ Direct_T1/Direct_demo.m
@@ -2,7 +2,7 @@ clear; clc; close all;
 
 %% Load files and preprocessing
 load('k_x90.mat');
-load('sMaps.mat');
+load('sMaps_old.mat');
 load('B1_x90.mat');
 load('supp.mat');
 load('ellipse.mat');
@@ -10,10 +10,10 @@ load('ellipse.mat');
 k                       = 500*k/max(abs(k(:)));
 [nx, ny, ns, nt, nr]    = size(k);
 k                       = k/sqrt(nx*ny);
-imgF                    = sum(repmat(conj(sMaps), [1 1 1 1 1]).*ifft2c(k), 5);
+% imgF                    = sum(repmat(conj(sMaps), [1 1 1 1 1]).*ifft2c(k), 5);
 
-opt.Initer              =100;
-opt.Outiter             =10;
+opt.Initer              = 500;
+opt.Outiter             = 10;
 
 %% Fully sampled maps
 refmat                  = 'ref.mat';
@@ -25,7 +25,7 @@ opt.B1                  = B1;
 opt.class               = 'single';
 if no_ref
     % Estimate references from fully sampled data using direct estimation
-    Mo                  = ones(nx, ny);
+    Mo                  = zeros(nx, ny);
     R1                  = ones(nx, ny);
     P                   = Mo;
     P(:, :, 2)          = R1;
@@ -47,15 +47,18 @@ opt.disp_err            = 1;
 opt.Mo                  = squeeze(Mo);
 opt.T1                  = squeeze(T1);
 opt.supp                = supp;
-opt.sMaps               = sMaps;
-opt.kfov                = repmat(nshift(kmask, [1 2]), [1 1 1 nt nr]);
-opt.kfov                = repmat(nshift(kmask, [1 2]), [1 1 1 nt nr]);
+opt.S                   = sMaps;
+opt.kfov                = repmat(kmask, [1 1 1 nt nr]);
 opt.pattern_type        = 2; % Sampling pattern footprint flag. 1: Rectangular. 2: Ellipsoid
+opt.FTdim           = [1 2];
+opt.FTshift           = 1;
 
 % Optional constraints.
 opt.wname               = 'db4'; % wavelet parameters
 opt.worder              = {[1 2],[1 2],[1 2]};
 opt.lambdaA             = [0 0 0 0];
+
+imgF                    = sqrt(sum(abs(sqrt(prod(opt.size(opt.FTdim)))*iFastFT(k, [1 2], 1)).^2, 5));
 
 %% Reconstruction script
 switch opt.pattern_type
@@ -72,17 +75,21 @@ switch opt.pattern_type
 end
 
 %% Reconstruction
-range = 1:17;
+range = 17;
 for ct = range % For 17 subsampling level in factor decreasing order
-    U1                  = repmat(Uyz(:, :, :, :, ct), [1 1 1 1 nr]);
-    U1                  = double(logical(nshift(U1, [1 2])));
+    U1                  = logical(repmat(Uyz(:, :, :, :, ct), [1 1 1 1 nr]));
+%     U1                  = double(logical(nshift(U1, [1 2])));
 
     kU                  = k.*U1;
     if opt.pattern_type == 2 % For ellipsoid footprint
         U1(~opt.kfov & U1 == 0) = 0.1;
     end
     
-    imgZF               = sum(repmat(conj(sMaps), [1 1 1 1 1]).*ifft2c(kU), 5);
+
+    opt.U               = U1;
+    
+%     imgZF               = sum(repmat(conj(sMaps), [1 1 1 1 1]).*ifft2c(kU), 5);
+    imgZF               = sum(conj(opt.S).*sqrt(prod(opt.size(opt.FTdim))).*iFastFT(kU, opt.FTdim, opt.FTshift), 5);
 
     figure(21);
     montage(abs(rot90(imgZF)), 'DisplayRange', [0 max(abs(imgZF(:)))], 'Size', [2 4]);
@@ -96,7 +103,7 @@ for ct = range % For 17 subsampling level in factor decreasing order
     P(:, :, 2)          = R1;
 
     tic,
-    [Mo, R1, iter, traces, cost] = P_SEN(P, sMaps, U1, kU, opt);
+    [Mo, R1, iter, traces, cost] = P_SEN(P, kU, opt);
     toc,
     T1                  = 1./R1;
     
